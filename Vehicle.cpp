215c85d39ceeab058203bca277181c9fcfbbc0d1
Zachary Seeley <zseeley7913@gmail.com>
2024-11-30 08:34:28 -0600

    Created class definition in Vehicle.h
    Created getter functions in Vehicle.cpp
    
    Created base functions for setters, the constructor, and the destructor in Vehicle.cpp

---
+++ Vehicle.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include "Vehicle.h"
+
+void Vehicle::setManufactureName(std::string input)
+{
+
+}
+
+std::string Vehicle::getManufactureName() const { return manufactureName; }
+
+void Vehicle::setModelName(std::string input)
+{
+
+}
+
+std::string Vehicle::getModelName() const { return modelName; }
+
+void Vehicle::setVehicleColor(std::string input)
+{
+
+}
+
+std::string Vehicle::getVehicleColor() const { return vehicleColor; }
+
+void Vehicle::setNumberTailLights(int num)
+{
+
+}
+
+int Vehicle::getNumberTailLights() const { return numberTailLights; }
+
+Vehicle::Vehicle()
+{
+
+}
+
+Vehicle::~Vehicle()
+{
+
+}
--- Vehicle.h
+++ Vehicle.h
@@ -1,1 +1,57 @@
 #pragma once
+
+//Vehicle class -- Accept and store information about a specific vehicle
+class Vehicle
+{
+public:
+	void setManufactureName(std::string input);
+	//sets the value of the private member string variable
+	//	manufactureName based on input string
+
+	std::string getManufactureName() const;
+	//returns the value of the private member string variable manufactureName
+
+	void setModelName(std::string input);
+	//sets the value of the private member string variable
+	//	modelName based on input string
+
+	std::string getModelName() const;
+	//returns the value of the private member string variable modelName
+
+	void setVehicleColor(std::string input);
+	//sets the value of the private member string variable
+	//	vehicleColor based on input string
+
+	std::string getVehicleColor() const;
+	//returns the value of the private member string variable vehicleColor
+
+	void setNumberTailLights(int num);
+	//sets the value of the private member int variable
+	//	numberTailLights based on randomly generated int input
+
+	int getNumberTailLights() const;
+	//returns the value of the private member int variable numberTailLights
+
+	Vehicle();
+	//default / zero argument constructor
+
+	~Vehicle();
+	//default destructor
+
+private:
+	std::string manufactureName;
+	//stores the name of the vehicle manufacturer
+	//	cannot be blank
+
+	std::string modelName;
+	//stores the name of the vehicle model
+	//	cannot be blank
+
+	std::string vehicleColor;
+	//stores the color of the vehicle
+	//	cannot be blank
+
+	int numberTailLights;
+	//stores the number of tail lights for the vehicle
+	//  random number between 1 and 5 inclusive
+};
