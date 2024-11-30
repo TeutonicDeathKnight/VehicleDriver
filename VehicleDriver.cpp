6abe969aa1b111b682aac4b69360c00fc393a4d4
Zachary Seeley <zseeley7913@gmail.com>
2024-11-30 08:10:28 -0600

    Add project files.

---
+++ VehicleDriver.cpp
@@ -0,0 +1,25 @@
+//VehicleDriver.cpp -- Store and display owned vehicle information
+//CSIS 212-<D02_202440>
+
+//include and using statements
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "Vehicle.h"
+
+using namespace std;
+
+//function prototypes
+
+
+//main()
+int main()
+{
+	cout << "Zachary Seeley -- Lab 6 - Dynamic Memory Assignment" << endl << endl;
+
+	//Closing program statements
+	system("pause");
+	return 0;
+}
+
+//function definitions
