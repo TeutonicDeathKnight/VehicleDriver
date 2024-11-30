bb9cd7fd3b36969d762bddcf3701b02c58c73c5c
Zachary Seeley <zseeley7913@gmail.com>
2024-11-30 08:52:17 -0600

    Created setters' logic in Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -4,42 +4,38 @@
 
 using namespace std;
 
-void Vehicle::setManufactureName(string input)
-{
-
-}
+void Vehicle::setManufactureName(string input) { manufactureName = input; }
 
 string Vehicle::getManufactureName() const { return manufactureName; }
 
-void Vehicle::setModelName(string input)
-{
-
-}
+void Vehicle::setModelName(string input) { modelName = input; }
 
 string Vehicle::getModelName() const { return modelName; }
 
-void Vehicle::setVehicleColor(string input)
-{
-
-}
+void Vehicle::setVehicleColor(string input) { vehicleColor = input; }
 
 string Vehicle::getVehicleColor() const { return vehicleColor; }
 
-void Vehicle::setNumberTailLights(int num)
-{
-
-}
+void Vehicle::setNumberTailLights(int num) { numberTailLights = num; }
 
 int Vehicle::getNumberTailLights() const { return numberTailLights; }
 
 Vehicle::Vehicle()
 {
+	cout << "Creating new Vehicle.\n\n";
+
 	manufactureName = "";
 	modelName = "";
 	vehicleColor = "";
 	numberTailLights = 0;
-	
-	cout << "Creating new Vehicle.\n\n";
 }
 
-Vehicle::~Vehicle() { cout << "In Vehicle destructor.\n"; }
+Vehicle::~Vehicle()
+{
+	cout << "In Vehicle destructor.\n";
+
+	delete& manufactureName;
+	delete& modelName;
+	delete& vehicleColor;
+	delete& numberTailLights;
+}
